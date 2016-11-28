85a2980678a4332b828f6f44e310c12deff9cf63
xelanos <Tomeror1478>
2016-11-28 15:10:57 +0200

    added EX3 question 3

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -14,15 +14,56 @@ def create_list():
             break
         else:
             user_strings_list.append(string_from_user)
-
     return user_strings_list
 
 
 def concat_list(str_list):
+    """A function that takes a list of strings and return one
+    string compromised from them all
+     """
     complete_user_string = ''
     for i in range(len(str_list)):
         complete_user_string = complete_user_string + str_list[i]
 
     return complete_user_string
 
-print(concat_list(create_list()))
+
+def list_sum(list_of_numbers):
+    """A function that takes a list of numbers
+    and returns their sum
+     """
+    total_sum = float()
+    for i in range(len(list_of_numbers)):
+        total_sum += float(list_of_numbers[i])
+    return total_sum
+
+
+def average(num_list):
+    """A function that takes a list of numbers
+        and returns their average
+         """
+    if len(num_list) == 0:
+        return None
+    else:
+        total_list_sum = list_sum(num_list)
+        # average is total sum divided by number of numbers
+        list_average = total_list_sum / len(num_list)
+        return list_average
+
+
+print(average([]))
+
+def cyclic(lst1,lst2):
+    pass
+
+def histogram(n,num_list):
+    pass
+
+def prime_factors(n):
+    pass
+
+def cartesian(lst1, lst2):
+    pass
+
+def pairs(n,num_list):
+    pass
