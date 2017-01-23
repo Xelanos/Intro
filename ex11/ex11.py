030dc751e524c47b0570f945c580fd3e1fd0c1f8
Xelanos <or3232@gmail.com>
2017-01-23 18:33:39 +0200

    Done v1.0

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -80,27 +80,29 @@ def solve(f, x0=-10000, x1=10000, epsilon=EPSILON):
     If no solution was found return None
     """
 
-    def binary_solve(g, starting_x, ending_x, stopping_width=0):
+    def binary_solve(g, starting_x, ending_x):
         """
         binary search to find solution in log time.
         checks for a solution between starting_x to ending_x
         :return: an x such a |f(x)|< epsilon (in float)
         """
-        if ending_x - starting_x <= stopping_width:
-            return None
-        else:
-            midpoint = (starting_x + ending_x) / 2
-            if abs(g(midpoint)) < epsilon:
-                return midpoint
+        while starting_x <= ending_x:
+            mid_point = (starting_x + ending_x) / 2
+            if math.isnan(mid_point):
+                return mid_point
+            if g(mid_point) <= -epsilon:
+                # if we are lower then -epsilon we need to search right side
+                starting_x = mid_point
+            elif g(mid_point) >= epsilon:
+                # if we are higher then epsilon we need to search left side
+                ending_x = mid_point
             else:
-                # if we are lower then -epsilon, we need to search to the right
-                if g(midpoint) < -epsilon:
-                    return binary_solve(g, midpoint, ending_x)
-                else:
-                # if we are higher then epsilon, we need to search to the left
-                    return binary_solve(g, starting_x, midpoint)
+                # if both are false we are in range, and therefore it's the
+                # solution
+                return mid_point
+        return ending_x if (f(ending_x) < f(starting_x)) else starting_x
 
-    if f(x0) * f(x1) >= 0:
+    if not f(x0) * f(x1) < 0:
         return None
     # If we have a monotonic up function we can use it for binary_solve.
     # if it is monotonic down, we take (-f) which is monotonic up with the same
@@ -112,39 +114,25 @@ def solve(f, x0=-10000, x1=10000, epsilon=EPSILON):
         return binary_solve(minus_f, x0, x1)
 
 
-def find_bounds(f, y):
-    x = 1
-    while f(x) < y:
-        x *= 2
-    low = 0 if (x == 1) else x/2
-    return low, x
-
-
-
 def inverse(g, epsilon=EPSILON):
     """return f s.t. f(g(x)) = x"""
-    def inverse_function(y):
+    def g_inverse(y):
         if y == 0:
-            upper_bound = 1
             lower_bound = -1
+            upper_bound = 1
         else:
-            lower_bound = min(y / 2, -y/2)
-            upper_bound = max(y / 2, -y/2)
+            upper_bound = max(y / 2, -y / 2)
+            lower_bound = min(y / 2, -y / 2)
 
         def function_to_solve(x): return g(x) - y
-
-        value = solve(function_to_solve, lower_bound, upper_bound, epsilon)
-        while not value:
+        sol = solve(function_to_solve, lower_bound, upper_bound, epsilon)
+        while sol is None:
             lower_bound += lower_bound / 2
             upper_bound += upper_bound / 2
-            value = solve(function_to_solve, lower_bound, upper_bound, epsilon)
-        return value
+            sol = solve(function_to_solve, lower_bound, upper_bound, epsilon)
+        return sol
 
-    return inverse_function
-
-
-
-inv = inverse(lambda x: x+1)
+    return g_inverse
 
 
 def compose(g, h):
@@ -197,17 +185,22 @@ def ex11_func_list():
     cos = derivative(sin_function())
     # function 0
     func_list.append(const_function(4))
+
     # function 1
-    func_list.append(sub_functions(const_function(3),sin_function()))
+    func_list.append(sub_functions(const_function(3), sin_function()))
+
     # function 2
     func_list.append(compose(sin_function(),
                              sub_functions(x, const_function(-2))))
+
     # function 3
     two_sin_x_square = sum_functions(two,
                                      sum_functions(sin_function(), x_square))
     func_list.append(div_functions(const_function(10), two_sin_x_square))
+
     # function 4
     func_list.append(div_functions(cos, sub_functions(sin_function(), two)))
+
     # function 5
     point_three_x_square = mul_functions(const_function(0.3), x_square)
     point_seven_x = mul_functions(const_function(0.7), x)
@@ -215,13 +208,18 @@ def ex11_func_list():
                             sub_functions(point_seven_x, const_function(-1)))
     func_list.append(mul_functions(const_function(-1),
                                    integral_function(quadric)))
+
     # function 6
     cos_sin = compose(cos, sin_function())
     point_three_cos = mul_functions(const_function(0.3), cos)
-    func_list.append(mul_functions(sub_functions(cos_sin, point_three_cos), 2))
+    func_list.append(mul_functions(sub_functions(cos_sin,point_three_cos),two))
+
     # function 7
+    x_qubed = mul_functions(x_square, identity())
+    func_to_inverse = sub_functions(const_function(2), x_qubed)
+    func_list.append(inverse(func_to_inverse))
 
-
+    return func_list
 
 
 # func that genrate the figure in the ex description
@@ -237,7 +235,6 @@ if __name__ == "__main__":
     # doctest.testmod()
     master = tk.Tk()
     graph = Graph(master, -10, -10, 10, 10)
-    plot_func(graph, inv, -10, 10)
     # un-tag the line below after implementation of plot_func
     # plot_func(graph,example_func,-10,10,SEGMENTS,'red')
     color_arr = ['black', 'blue', 'red', 'green', 'brown', 'purple',
